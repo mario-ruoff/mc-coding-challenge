466d54715959a659599d8ab326d2a7ad16f359e0
mario-ruoff <mario.srx99@gmail.com>
2025-02-09 17:05:06 +0000

    Add switching between upload form and table view

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react"
+import { useState } from "react"
 import { ThemeProvider } from "@/components/common/theme-provider"
 import { ModeToggle } from './components/common/mode-toggle'
 import { DataTable } from "@/components/common/data-table"
@@ -9,50 +9,71 @@ import "./App.css"
 
 function App() {
   const [data, setData] = useState([])
-  const [file, setFile] = useState<File | null>(null)
   const [error, setError] = useState<string | null>(null)
 
-  const handleFileChange = (file: File | null) => {
-    setFile(file)
-  }
+  const handleDataChange = (file: File | null) => {
 
-  useEffect(() => {
-    if (file) {
+    // If file is removed
+    if (!file) {
+      setData([])
+      setError(null);
+    }
+
+    // If file is uploaded
+    else {
       const reader = new FileReader();
       reader.onload = (fileEvent) => {
         if (fileEvent.target) {
           try {
+            // Parse the JSON data from the file
             const json = JSON.parse(fileEvent.target.result as string);
             setData(json);
             setError(null);
           } catch (error) {
             console.error("Error parsing JSON:", error);
             setError("JSON could not be parsed. Please check the file format.");
-            setFile(null);
           }
         }
       };
       reader.readAsText(file);
     }
-  }, [file]);
+  }
 
   return (
     <ThemeProvider defaultTheme="dark" storageKey="ui-theme">
-      <h1>Interactive JSON Viewer</h1>
-      {data.length > 20
-        ? <DataTable data={data} />
-        : <>
-          <FileUploader file={file} handleFileChange={handleFileChange} accept={{ "application/json": [] }} />
+
+
+      {/* Page title */}
+      <div className="fixed top-0 w-full text-center py-12">
+        <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">Interactive JSON Viewer</h1>
+      </div>
+
+      {/* Theme switcher component */}
+      <div className="fixed top-0 right-0 p-4">
+        <ModeToggle />
+      </div>
+
+      <div className="flex min-h-svh w-full items-center justify-center">
+        <div className="w-full max-w-2xl">
+
+          {/* File dropzone component */}
+          <FileUploader handleDataChange={handleDataChange} accept={{ "application/json": [] }} />
+
+          {/* Error section */}
           {error && (
-            <Alert variant="destructive">
+            <Alert variant="destructive" className="mt-4">
               <AlertCircle />
-              <AlertTitle>Error</AlertTitle>
-              <AlertDescription>{error}</AlertDescription>
+              <AlertTitle className="ml-4">Error</AlertTitle>
+              <AlertDescription className="ml-4">{error}</AlertDescription>
             </Alert>
           )}
-        </>
-      }
-      <ModeToggle />
+
+          {/* JSON data table */}
+          {data.length > 0 && <DataTable data={data} />}
+
+
+        </div>
+      </div>
     </ThemeProvider>
   )
 }
--- app/src/components/common/file-uploader.tsx
+++ app/src/components/common/file-uploader.tsx
@@ -1,99 +1,125 @@
+import { useState } from "react"
 import { Upload, X } from "lucide-react"
 import Dropzone from "react-dropzone"
-
 import { cn } from "@/lib/utils"
 import { Button } from "@/components/ui/button"
+import { Card, CardDescription, CardHeader, CardTitle } from "@/components/ui/card"
 
 interface FileUploaderProps {
-  file: File | null;
-  handleFileChange: (file: File | null) => void;
+  handleDataChange: (data: File | null) => void;
   accept?: { [key: string]: string[] };
 }
 
-export function FileUploader({ file, handleFileChange, accept = {}, ...dropzoneProps }: FileUploaderProps) {
+export function FileUploader({handleDataChange, accept = {}, ...dropzoneProps }: FileUploaderProps) {
 
+  const [file, setFile] = useState<File | null>(null)
   const isDisabled = !!file
+  
+  // Function to handle file change event
+  const handleFileChange = (file: File | null) => {
+    setFile(file)
+    handleDataChange(file)
+  }
 
   return (
-    <div className="">
-      <Dropzone
-        onDrop={(acceptedFiles: File[]) => {handleFileChange(acceptedFiles[0])}}
-        accept={accept}
-        maxFiles={1}
-        multiple={false}
-        disabled={isDisabled}
-      >
-        {({ getRootProps, getInputProps, isDragActive }) => (
-          <div
-            {...getRootProps()}
-            className={cn(
-              "group relative grid h-52 w-full cursor-pointer place-items-center rounded-lg border-2 border-dashed border-muted-foreground/25 px-5 py-2.5 text-center transition hover:bg-muted/25",
-              "ring-offset-background focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2",
-              isDragActive && "border-muted-foreground/50",
-              isDisabled && "pointer-events-none opacity-60",
-            )}
-            {...dropzoneProps}
+    <>
+      {file
+        ? (
+            <Card className="bg-secondary mb-8">
+            <CardHeader className="w-full grid grid-cols-3 gap-2 text-left pt-2 pb-3 px-4">
+              <CardTitle className="mt-2">{file.name}</CardTitle>
+              <CardDescription className="">{file.size} bytes</CardDescription>
+                <div className="col-start-3 flex justify-end">
+                <Button
+                  type="button"
+                  variant="outline"
+                  size="icon"
+                  className="size-7"
+                  onClick={() => handleFileChange(null)}
+                >
+                  <X className="size-4" aria-hidden="true" />
+                  <span className="sr-only">Remove file</span>
+                </Button>
+                </div>
+            </CardHeader>
+          </Card>
+        // <div className="relative flex items-center gap-2.5 mt-4">
+        //     <div className="flex flex-1">
+        //         <div className="w-full grid grid-cols-2 gap-2 text-left">
+        //           <p className="text-sm font-medium text-foreground/80">
+        //             {file.name}
+        //           </p>
+        //           <p className="text-xs text-muted-foreground">
+        //             {file.size} bytes
+        //           </p>
+        //           </div>
+        //     </div>
+        //     <div className="flex items-center gap-2">
+        //       <Button
+        //         type="button"
+        //         variant="outline"
+        //         size="icon"
+        //         className="size-7"
+        //         onClick={() => handleFileChange(null)}
+        //       >
+        //         <X className="size-4" aria-hidden="true" />
+        //         <span className="sr-only">Remove file</span>
+        //       </Button>
+        //     </div>
+        //   </div>
+          )
+        : (
+          <Dropzone
+            onDrop={(acceptedFiles: File[]) => { handleFileChange(acceptedFiles[0]) }}
+            accept={accept}
+            maxFiles={1}
+            multiple={false}
+            disabled={isDisabled}
           >
-            <input {...getInputProps()} />
-            {isDragActive ? (
-              <div className="flex flex-col items-center justify-center gap-4 sm:px-5">
-                <div className="rounded-full border border-dashed p-3">
-                  <Upload
-                    className="size-7 text-muted-foreground"
-                    aria-hidden="true"
-                  />
-                </div>
-                <p className="font-medium text-muted-foreground">
-                  Drop the file here
-                </p>
-              </div>
-            ) : (
-              <div className="flex flex-col items-center justify-center gap-4 sm:px-5">
-                <div className="rounded-full border border-dashed p-3">
-                  <Upload
-                    className="size-7 text-muted-foreground"
-                    aria-hidden="true"
-                  />
-                </div>
-                <div className="flex flex-col gap-px">
-                  <p className="font-medium text-muted-foreground">
-                    Drag {`'n'`} drop file here, or click to select file
-                  </p>
-                </div>
+            {({ getRootProps, getInputProps, isDragActive }) => (
+              <div
+                {...getRootProps()}
+                className={cn(
+                  "group relative grid h-52 w-full cursor-pointer place-items-center rounded-lg border-2 border-dashed border-muted-foreground/25 px-5 py-2.5 text-center transition hover:bg-muted/25",
+                  "ring-offset-background focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2",
+                  isDragActive && "border-muted-foreground/50",
+                  isDisabled && "pointer-events-none opacity-60",
+                )}
+                {...dropzoneProps}
+              >
+                <input {...getInputProps()} />
+                {isDragActive ? (
+                  <div className="flex flex-col items-center justify-center gap-4 sm:px-5">
+                    <div className="rounded-full border border-dashed p-3">
+                      <Upload
+                        className="size-7 text-muted-foreground"
+                        aria-hidden="true"
+                      />
+                    </div>
+                    <p className="font-medium text-muted-foreground">
+                      Drop the file here
+                    </p>
+                  </div>
+                ) : (
+                  <div className="flex flex-col items-center justify-center gap-4 sm:px-5">
+                    <div className="rounded-full border border-dashed p-3">
+                      <Upload
+                        className="size-7 text-muted-foreground"
+                        aria-hidden="true"
+                      />
+                    </div>
+                    <div className="flex flex-col gap-px">
+                      <p className="font-medium text-muted-foreground">
+                        Drag {`'n'`} drop file here, or click to select file
+                      </p>
+                    </div>
+                  </div>
+                )}
               </div>
             )}
-          </div>
+          </Dropzone>
         )}
-      </Dropzone>
-      {file && (
-        <div className="relative flex items-center gap-2.5 mt-4">
-          <div className="flex flex-1 gap-2.5">
-            <div className="flex w-full flex-col gap-2">
-              <div className="flex flex-col gap-px">
-                <p className="line-clamp-1 text-sm font-medium text-foreground/80">
-                  {file.name}
-                </p>
-                <p className="text-xs text-muted-foreground">
-                  {file.size} bytes
-                </p>
-              </div>
-            </div>
-          </div>
-          <div className="flex items-center gap-2">
-            <Button
-              type="button"
-              variant="outline"
-              size="icon"
-              className="size-7"
-              onClick={() => handleFileChange(null)}
-            >
-              <X className="size-4" aria-hidden="true" />
-              <span className="sr-only">Remove file</span>
-            </Button>
-          </div>
-        </div>
-      )}
-    </div>
+    </>
   )
 }
 
